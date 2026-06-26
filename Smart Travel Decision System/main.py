c64bf5742fab5884dc10c5ee97835816f85173f9
ArsalJan34 <arsaljaan532@gmail.com>
2026-06-26 21:12:18 +0500

    creating Smart Travel Decision System using if elif else statements

---
+++ Smart Travel Decision System/main.py
@@ -0,0 +1,4 @@
+print("===============Welcome to Travel Decision System=============")
+Distance = int(input("How far is your destination? : "))
+Raining = input("Is it raining (YES or NO) : ").lower()
+Bicycle = input("Do you have bicycle (YES or NO) : ")
