0d7c06248c75874794e0e1e21bdb457cfa2b23d6
ArsalJan34 <arsaljaan532@gmail.com>
2026-06-26 21:38:15 +0500

    i used == yes in inputs so i can just boolean in conditional statements like if elif and else first i was using only comparison operators and comparing them to the input of user while not using true or false the boolean values

--- Smart Travel Decision System/main.py
+++ Smart Travel Decision System/main.py
@@ -1,4 +1,24 @@
 print("===============Welcome to Travel Decision System=============")
 Distance = int(input("How far is your destination? : "))
-Raining = input("Is it raining (YES or NO) : ").lower()
-Bicycle = input("Do you have bicycle (YES or NO) : ")
+
+Raining = input("Is it raining (YES or NO): ").lower() == "yes"
+Bicycle = input("Do you have a bicycle (YES or NO): ").lower() == "yes"
+Car = input("Do you have a car (YES or NO): ").lower() == "yes"
+Application = input("Do you have a Ride Share application (YES or NO): ").lower() == "yes"
+if Distance <= 1:
+    if not Raining:
+        print("You are recommended to walk.")
+    else:
+        print("Walking is not recommended.")
+
+elif Distance <= 6:
+    if Bicycle and not Raining:
+        print("You are recommended to travel by bicycle.")
+    else:
+        print("Cycling is not recommended.")
+
+else:
+    if Car or Application:
+        print("You are recommended to travel by car or ride share.")
+    else:
+        print("There is no suitable transportation available.")
