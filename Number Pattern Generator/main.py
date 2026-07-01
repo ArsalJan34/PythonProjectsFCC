1c42c34c2d4db787aed933eafe60fdf47247f8fe
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-01 17:56:12 +0500

    Created number pattern generator

---
+++ Number Pattern Generator/main.py
@@ -0,0 +1,11 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n <= 0:
+        return "Argument must be an integer greater than 0."
+    number = []
+    for i in range(1, n + 1):
+        number.append(str(i))
+    return " ".join(number)
+num = number_pattern(4)
+print(num)
