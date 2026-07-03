95aca07c56a98623d760f89b0fa22b137685c6d9
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-04 02:11:11 +0500

    updated:

--- Student Exam Analyzer/main.py
+++ Student Exam Analyzer/main.py
@@ -14,6 +14,27 @@ def exam_analyzer(students):
     if len(parts) != "2":
       invalid_students += 1
       continue
-
+    name = parts[0]
+    marks = parts[1]
+    if not marks.isdigit():
+      invalid_students += 1
+      continue
+    marks = int(marks)
+    if not marks > 0 and marks < 100:
+      invalid_students += 1
+      continue
+    valid_students += 1
+    if marks >= 90:
+      grade = "A"
+      A_grades += 1
+    elif marks >= 80:
+      grade = "B"
+    elif marks >= 70:
+      grade = "C"
+    elif marks >= 60:
+      grade = "D"
+    else:
+      grade = "F"
+      total_failed += 1
 
 
