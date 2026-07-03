0f2cef31924e7ce0321ffd2174a9660a78e2c501
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-04 01:26:43 +0500

    Student exam analyzer

--- Password Strength Checker/main.py
+++ Password Strength Checker/main.py
@@ -1,24 +1,30 @@
 def password_checker(passwords):
     results = []
+
     for password in passwords:
         has_upper = False
         has_lower = False
         has_digit = False
         length_ok = False
+
         if len(password) >= 8:
             length_ok = True
+
         index = 0
         while index < len(password):
             character = password[index]
+
             if character.isupper():
                 has_upper = True
             elif character.islower():
                 has_lower = True
             elif character.isdigit():
                 has_digit = True
+
             index += 1
-        # Count satisfied conditions
+
         score = 0
+
         if has_upper:
             score += 1
         if has_lower:
@@ -27,7 +33,7 @@ def password_checker(passwords):
             score += 1
         if length_ok:
             score += 1
-        # Decide password strength
+
         if score == 4:
             results.append("Strong")
         elif score == 3:
@@ -36,13 +42,15 @@ def password_checker(passwords):
             results.append("Weak")
         else:
             results.append("Very Weak")
+
     return results
+
 passwords = []
-num = int(input("How many passwords do you want to check? "))
+num = int(input("How many passwords do you want to check ? "))
 for i in range(num):
-    password = input(f"Enter password {i + 1}: ")
-    passwords.append(password)
+  password = input(f"Enter password {i +1 }:")
+  passwords.append(password)
 strengths = password_checker(passwords)
-print("\nPassword Strength Results:")
+print("\n Password Strength Results: ")
 for i, strength in enumerate(strengths):
-    print(f"Password {i + 1}: {strength}")
+  print(f"Password {i + 1}: {strength} ")
---
+++ Student Exam Analyzer/main.py
@@ -0,0 +1,7 @@
+def exam_analyzer(students):
+  valid_students = 0
+  invalid_students = 0
+  A_grades = 0
+  Failed_students = 0
+  grades = []
+  for student in students:
