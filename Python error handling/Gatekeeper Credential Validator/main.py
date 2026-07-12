0e6f1d7be4f6e0849370cc4a701333ff3c0edd2b
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-13 00:54:12 +0500

    Gatekeeper Credential Validator

--- Python error handling/Gatekeeper Credential Validator/main.py
+++ Python error handling/Gatekeeper Credential Validator/main.py
@@ -1,3 +1,21 @@
-# lets  create a custom excption
-class InvalidCredentialError(Exception):
-  pass
+# Creating a custom exception class by inheriting from Exception
+class InvalidCredentialsError(Exception):
+    pass
+def secure_gatekeeper(username, password):
+    correct_username = "admin"
+    correct_password = "password123"
+    if username == correct_username and password == correct_password:
+         return "Access Granted"
+    else:
+         raise InvalidCredentialsError("Access Denied")
+
+try:
+    result = secure_gatekeeper("admin", "password123")
+    print(result)
+except InvalidCredentialsError as e:
+    print(f"Error caught: {e}")
+try:
+    result = secure_gatekeeper("admin", "wrong_password")
+    print(result)
+except InvalidCredentialsError as e:
+    print(f"Error caught : {e}")  # Prints: Error caught: Access Denied
