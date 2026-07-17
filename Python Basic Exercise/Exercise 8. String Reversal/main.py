2f9364025ad3705a9e2220a79521b736308d8196
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-18 01:18:58 +0500

    Exercise 8. String Reversal

---
+++ Python Basic Exercise/Exercise 8. String Reversal/main.py
@@ -0,0 +1,6 @@
+text = "Python"
+# slicing the string from start to finish adding -1 at end to step backward by 1 character at time
+reversed_text = text[::-1]
+
+print(f" original text :  {text}")
+print(f" reversed text : {reversed_text}")
