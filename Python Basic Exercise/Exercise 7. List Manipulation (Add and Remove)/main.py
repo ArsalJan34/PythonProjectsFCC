93102a88764b048c365e630d9e48013a7b44d9b4
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-18 01:13:30 +0500

    Exercise 7. List Manipulation: Add and Remove

---
+++ Python Basic Exercise/Exercise 7. List Manipulation (Add and Remove)/main.py
@@ -0,0 +1,7 @@
+Fruits = ["apple", "orange", "bannana", "date", "grapes"]
+# add fruit at the end
+Fruits.append("Fig")
+print(Fruits)
+# Remove fruit from seocnd index
+Fruits.pop(1)
+print(Fruits)
