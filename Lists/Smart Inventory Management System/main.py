be4cb7ecaf8a1a1d933284944b64df068e6a1d4e
ArsalJan34 <arsaljaan532@gmail.com>
2026-07-25 16:21:25 +0500

    practicing lists

---
+++ Lists/Smart Inventory Management System/main.py
@@ -0,0 +1,15 @@
+inventory = ["Laptop", "mouse", "keyboard", "monitor"]
+new_shipment = ["headset","webcam"]
+# using extend() to add multipele items from another list
+inventory.extend(new_shipment)
+
+# here using insert() to add high priority item at the top (index 0)
+inventory.insert(0,"server_rack")
+
+# using append to add single item at end
+inventory.append("docking_station")
+
+# usign copy() to keep a backup befroe modifying further
+backup_inventory = inventory.copy()
+
+# pop() and remove() using them to process item removals
